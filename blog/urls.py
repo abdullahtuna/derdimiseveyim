dcc64fd149ac3c20006194829cd58eca11a03a11
Abdullah Tuna <abdullahtuna90@gmail.com>
2023-09-20 22:54:15 +0300

    Kullanıcıların post göndermesi, silmesi ve düzenlemesi sağlandı.

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,11 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+
     path('', views.PostList.as_view(), name='home'),
+    path('post/create/', views.PostCreate.as_view(), name='post_create'),
+    path('post/<int:pk>/', views.PostUpdate.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
     path('about/', views.about, name='about'),
     path('iletisim/', views.iletisim, name='iletisim'),
     path('kurallar/', views.kurallar, name='kurallar'),
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,14 @@ from django.http.response import HttpResponse
 from derdimiseveyim.forms import NewUserForm
 from django.contrib.auth import login
 from django.contrib import messages
-
+from django.shortcuts import render, redirect
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.utils.text import slugify
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 def about(request):
     return render(request, 'about.html')
@@ -13,6 +20,7 @@ def iletisim(request):
     return render(request, 'iletisim.html')
 def kurallar(request):
     return render(request, 'kurallar.html')
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -22,7 +30,55 @@ class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+# ...
+
+class PostCreate(LoginRequiredMixin, generic.CreateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = 'post_form.html'
+
+    def get_success_url(self):
+        messages.success(
+            self.request, 'Derdiniz Başarıyla Sevildi.')
+        return reverse_lazy('home')
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.slug = slugify(form.cleaned_data['title'])
+        obj.save()
+        return super().form_valid(form)
 
 
+class PostUpdate(LoginRequiredMixin, generic.UpdateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = 'post_form.html'
 
-# Create your views here.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        update = True
+        context['update'] = update
+
+        return context
+
+    def get_success_url(self):
+        messages.success(
+            self.request, 'Derdiniz Başarıyla Tekrar Sevildi.')
+        return reverse_lazy('home')
+
+    def get_queryset(self):
+        return self.model.objects.filter(author=self.request.user)
+
+
+class PostDelete(LoginRequiredMixin, generic.DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+
+    def get_success_url(self):
+        messages.success(
+            self.request, 'Derdiniz Çöpe Gitti.')
+        return reverse_lazy('home')
+
+    def get_queryset(self):
+        return self.model.objects.filter(author=self.request.user)
