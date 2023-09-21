54346798b977fea6cc189bb3b598b2594605a2d4
Abdullah Tuna <abdullahtuna90@gmail.com>
2023-09-22 00:44:01 +0300

    Yorum bölümü eklendi.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import generic
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
 from django.http.response import HttpResponse
 from derdimiseveyim.forms import NewUserForm
 from django.contrib.auth import login
@@ -82,3 +83,48 @@ class PostDelete(LoginRequiredMixin, generic.DeleteView):
 
     def get_queryset(self):
         return self.model.objects.filter(author=self.request.user)
+
+class PostView(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs["pk"]
+        slug = self.kwargs["slug"]
+
+        form = CommentForm()
+        post = get_object_or_404(Post, pk=pk, slug=slug)
+        comments = post.comment_set.all()
+
+        context['post'] = post
+        context['comments'] = comments
+        context['form'] = form
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        self.object = self.get_object()
+        context = super().get_context_data(**kwargs)
+
+        post = Post.objects.filter(id=self.kwargs['pk'])[0]
+        comments = post.comment_set.all()
+
+        context['post'] = post
+        context['comments'] = comments
+        context['form'] = form
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            content = form.cleaned_data['content']
+
+            comment = Comment.objects.create(
+                name=name, email=email, content=content, post=post
+            )
+
+            form = CommentForm()
+            context['form'] = form
+            return self.render_to_response(context=context)
+
+        return self.render_to_response(context=context)
